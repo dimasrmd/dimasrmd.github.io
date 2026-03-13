247646a2e10405c1ec2524383b7d734d903e82e8
Dimas Ramadhani <dimasramadhani330@gmail.com>
2026-03-14 05:22:50 +0700

    Perbaikan Konfigurasi Untuk Bisa di Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,21 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+// // https://astro.build/config
+// export default defineConfig({
+//   site: 'https://example.com',
+//   integrations: [mdx(), sitemap()],
 
+//   vite: {
+//     plugins: [tailwindcss()],
+//   },
+// });
+
+export default defineConfig({
+  // Sesuaikan dengan username dan nama repo kamu
+  site: 'https://dimasrmd.github.io',
+  base: '/BLOG-JURNAL-HARIAN-MAHASISWA', 
+  integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
